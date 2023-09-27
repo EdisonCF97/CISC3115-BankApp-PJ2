2c5c6760124c036749aff875dc52ac6df98a5372
Edison Cen Feng <cenfengedison1997@gmail.com>
2023-09-27 13:59:59 -0400

    BankApp.java

--- src/main/java/BankApp.java
+++ src/main/java/BankApp.java
@@ -34,6 +34,26 @@ public class BankApp{
                 appLoop(account);
     }
         }
+
+        public static void displayMenu(CheckingAccount account){
+            System.out.println("Account No. " + account.getAccountNo());
+            System.out.println("1. Check balance");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Deposit");
+            System.out.println("0. Exit");
+            System.out.print("Enter Option: ");
+        }
+
+
+        public static Bank loadBankAccounts() {
+            Bank bank = new Bank(5);
+            bank.addAccount("123456789", "pw789", BigDecimal.valueOf(5000.00));
+            bank.addAccount("123456788", "pw788", BigDecimal.valueOf(50000.00));
+            bank.addAccount("123456787", "pw787", BigDecimal.valueOf(10000.00));
+            bank.addAccount("123456786", "pw786", BigDecimal.valueOf(20000.00));
+            bank.addAccount("123456785", "pw785", BigDecimal.valueOf(30000.00));
+            return bank;
+        }
     
         public static void appLoop(CheckingAccount account) {
             Scanner scanner = new Scanner(System.in);
@@ -69,27 +89,7 @@ public class BankApp{
                     System.out.println("Invalid option. Please try again.");
             }
         }
-    }
-
-        public static void displayMenu(CheckingAccount account){
-            System.out.println("Account No. " + account.getAccountNo());
-            System.out.println("1. Check balance");
-            System.out.println("2. Withdraw");
-            System.out.println("3. Deposit");
-            System.out.println("0. Exit");
-            System.out.print("Enter Option: ");
-        }
-
-
-        public static Bank loadBankAccounts() {
-            Bank bank = new Bank(5);
-            bank.addAccount("123456789", "pw789", BigDecimal.valueOf(5000.00));
-            bank.addAccount("123456788", "pw788", BigDecimal.valueOf(50000.00));
-            bank.addAccount("123456787", "pw787", BigDecimal.valueOf(10000.00));
-            bank.addAccount("123456786", "pw786", BigDecimal.valueOf(20000.00));
-            bank.addAccount("123456785", "pw785", BigDecimal.valueOf(30000.00));
-            return bank;
-        }
+    }     
 }
 
 
