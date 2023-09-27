cae71c308fc405de5b25886c85a6b8420ac6aa44
Edison Cen Feng <cenfengedison1997@gmail.com>
2023-09-26 22:47:57 -0400

    CheckingAccount.java

--- src/main/java/CheckingAccount.java
+++ src/main/java/CheckingAccount.java
@@ -43,7 +43,7 @@ public class CheckingAccount {
 }
 
   public boolean matchAccount(String ssn, String pw){
-        return this.ssn.equals(ssn) && this.pwHash.equals(pw);
+        return this.ssn.equals(ssn) && this.pw.equals(pw);
   }
 
   public long getAccountNo(){
