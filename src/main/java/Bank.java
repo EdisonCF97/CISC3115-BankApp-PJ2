1d4b064162ac75e33b6df73d7be2b58ce1554f0c
Edison Cen Feng <cenfengedison1997@gmail.com>
2023-09-26 22:22:12 -0400

    Bank.java

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -36,10 +36,10 @@ public class Bank {
     public CheckingAccount getAuthorizedCheckingAccount(String ssn, String pw) {
         for (int i = 0; i < numAccounts; i++) {
             if (checkingAccounts[i].matchAccount(ssn, pw)) {
-                return true;
+                return checkingAccounts[i];
             }
         }
-        return false;
+        return null;
     }
 
     public boolean hasAccountFor(String ssn) {
