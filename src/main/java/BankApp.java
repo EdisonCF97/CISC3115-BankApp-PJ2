6ead5d424e64fab304e0df0076fbb0b60774368d
Edison Cen Feng <cenfengedison1997@gmail.com>
2023-09-27 20:47:15 -0400

    BankApp.java

--- src/main/java/BankApp.java
+++ src/main/java/BankApp.java
@@ -3,6 +3,8 @@ import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
+import main.java.Bank;
+
 
 public class BankApp{
 
@@ -21,22 +23,27 @@ public class BankApp{
             System.out.print("Enter password: ");
             String pw = scanner.nextLine();
             int option;*/
-
             Bank bank = loadBankAccounts();
+            Scanner scanner = new Scanner(System.in);
 
-            try (Scanner scanner = new Scanner(System.in)) {
+
+
+                
+        /*CheckingAccount account = new CheckingAccount("6637862074333514921", "pwHashHere", 123456, BigDecimal.valueOf(5000.00));
+  */    
                 System.out.print("Enter SSN: ");
                 String ssn = scanner.nextLine();
                 System.out.print("Enter password: ");
                 String pw = scanner.nextLine();
-                /*CheckingAccount account = new CheckingAccount("6637862074333514921", "pwHashHere", 123456, BigDecimal.valueOf(5000.00));
-    */          CheckingAccount account = bank.getAuthorizedCheckingAccount(ssn, pw);
+
+                CheckingAccount account = bank.getAuthorizedCheckingAccount(ssn, pw);
 
                 appLoop(account);
-            }
-        }
 
-        public static void displayMenu(CheckingAccount account){
+            }
+
+                
+        public static void displayMenu(CheckingAccount account){           
             System.out.println("Account No. " + account.getAccountNo());
             System.out.println("1. Check balance");
             System.out.println("2. Withdraw");
@@ -45,10 +52,9 @@ public class BankApp{
         }
         
         public static void appLoop(CheckingAccount account) {
-            Scanner scanner = new Scanner(System.in);
             int option;
-         
-        
+            
+            
         while (true) {
             displayMenu(account);
             System.out.print("Enter Option: ");
